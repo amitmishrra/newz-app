6dac2acd67d016d799a3fe6741d785e7f08b8838
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2022-11-23 23:12:58 +0530

    bugs fixed 3

--- src/Components/NewsComponent/News.jsx
+++ src/Components/NewsComponent/News.jsx
@@ -4,62 +4,60 @@ import "./news.css"
 import CircularProgress from '@mui/material/CircularProgress';
 export default function News(props) {
   const [page, setPage] = useState(1);
-
-  const [loading, setLoading] = useState(false)
-const [category, setCategory] = useState("general");
-
-// apiKey = "57f823bf1d9e487c8e841024c2a5bdb2"
-let apiKey = "f138ef5fed654a6a90b8f7171ffa589a"
-
-useEffect(()=>{
-  
-  if(window.location.pathname=="/general"){
-    setCategory("general"); 
-    console.log(category)
-    setLoading(false)
-
- }
-   if(window.location.pathname=="/business"){
-    setCategory("business");
-    console.log(category)
-    setLoading(false)
- }
-   if(window.location.pathname=="/entertainment"){
-    setCategory("entertainment");
-    console.log(category)
-    setLoading(false)
- }
-   if(window.location.pathname=="/technology"){
-    setCategory("technology");
-    console.log(category)
-    setLoading(false)
- }
-   if(window.location.pathname=="/health"){
-    setCategory("health");
-    console.log(category)
-    setLoading(false)
- }
-   if(window.location.pathname=="/science"){
-    setCategory("science");
-    console.log(category)
-    setLoading(false)
- }
-   if(window.location.pathname=="/"){
-    setCategory("sports");
-    console.log(category)
-    setLoading(false)
- }
-
-},[category, window.location.pathname])
-
-
-
-
-
   const [country, setCountry] = useState("in");
   const [news, setNews] = useState([]);
   const [total, setTotal] = useState();
+  let apiKey = "f138ef5fed654a6a90b8f7171ffa589a"
   let api = `https://newsapi.org/v2/top-headlines?country=${country}&category=${props.category}&apiKey=${apiKey}&page=${page}&pageSize=20`;
+  const [loading, setLoading] = useState(false)
+  const [category, setCategory] = useState("general");
+
+
+  useEffect(() => {
+
+    if (window.location.pathname == "/general") {
+      setCategory("general");
+      console.log(category)
+      setLoading(false)
+
+    }
+    if (window.location.pathname == "/business") {
+      setCategory("business");
+      console.log(category)
+      setLoading(false)
+    }
+    if (window.location.pathname == "/entertainment") {
+      setCategory("entertainment");
+      console.log(category)
+      setLoading(false)
+    }
+    if (window.location.pathname == "/technology") {
+      setCategory("technology");
+      console.log(category)
+      setLoading(false)
+    }
+    if (window.location.pathname == "/health") {
+      setCategory("health");
+      console.log(category)
+      setLoading(false)
+    }
+    if (window.location.pathname == "/science") {
+      setCategory("science");
+      console.log(category)
+      setLoading(false)
+    }
+    if (window.location.pathname == "/") {
+      setCategory("sports");
+      console.log(category)
+      setLoading(false)
+    }
+
+  }, [category, window.location.pathname])
+
+
+
+
+
 
   useEffect(() => {
     fetch(api).
@@ -68,30 +66,30 @@ useEffect(()=>{
         setNews(resp.articles)
         setTotal(resp.totalResults)
       }).then(
-        setTimeout(()=>{
+        setTimeout(() => {
           setLoading(true)
         }, 3000)
       )
 
-  }, [category, api ,page])
+  }, [category, api, page])
 
 
-  let totalPages = Math.ceil(total/20);
+  let totalPages = Math.ceil(total / 20);
 
   const handlePrev = async () => {
-      setPage(page - 1);
-      setLoading(false)
-      console.log(page);
-     
+    setPage(page - 1);
+    setLoading(false)
+    console.log(page);
+
   }
 
   const handleNext = async () => {
-      setPage(page + 1);
-      setLoading(false)
-      console.log(page);
-    }
+    setPage(page + 1);
+    setLoading(false)
+    console.log(page);
+  }
 
-  
+
 
 
 
@@ -99,33 +97,33 @@ useEffect(()=>{
     <>
       <div className="news-box">
         {
-          loading && news.length > 1?
+          loading && news ?
 
-          news.map((element) => {
-            return (
-              <>
-                <NewsItem title={element.title} image={element.urlToImage} description={element.description} link={element.url}
-                author={element.author} date={element.publishedAt.slice(0,10)} time={element.publishedAt.slice(11,19)}
-                />
-              </>
-            )
-          }) : 
-         <div className='loader'>
-          <div className="loadingText">
-          Loading News....
-          </div>
-         
-         <div className="circleContainer">
-         <CircularProgress className='circularLoader'  />
-         </div>
-         </div>
+            news.map((element) => {
+              return (
+                <>
+                  <NewsItem title={element.title} image={element.urlToImage} description={element.description} link={element.url}
+                    author={element.author} date={element.publishedAt.slice(0, 10)} time={element.publishedAt.slice(11, 19)}
+                  />
+                </>
+              )
+            }) :
+            <div className='loader'>
+              <div className="loadingText">
+                Loading News....
+              </div>
+
+              <div className="circleContainer">
+                <CircularProgress className='circularLoader' />
+              </div>
+            </div>
         }
       </div>
 
       <div className="news-buttons">
         <button disabled={page == 1} onClick={handlePrev} className='buttons'>	&#8592; Previous</button>
         {page}/{totalPages}
-        <button  disabled={page == totalPages}   onClick={handleNext} className='buttons'>Next 	&#8594;</button>
+        <button disabled={page == totalPages} onClick={handleNext} className='buttons'>Next 	&#8594;</button>
       </div>
 
     </>
